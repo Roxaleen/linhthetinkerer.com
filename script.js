5d23e936cd60861063e24f7678cad1dcf4a737b3
Roxaleen <nttulinh1995@gmail.com>
2024-08-29 19:05:00 +0800

    navmenu updates!

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 // Navigation menu configuration
 const navMenu = document.getElementById('navbar-links');
 const navBtn = document.getElementById('nav-menu-button');
+const navBackdrop = document.getElementById('nav-menu-backdrop');
 
 function toggleNavMenu() {
     if(navMenu.getAttribute('aria-expanded') === 'true') {
         navMenu.setAttribute('aria-expanded','false');
-        // document.removeEventListener('click',detectExternalClick);
     } else {
         navMenu.setAttribute('aria-expanded','true');
-        // document.addEventListener('click',detectExternalClick);
+        navBtn.setAttribute('aria-label','Close navigation menu');
     }
 }
 
 navBtn.addEventListener('click', toggleNavMenu);
+navBackdrop.addEventListener('click', toggleNavMenu);
+navMenu.addEventListener('blur', toggleNavMenu);
